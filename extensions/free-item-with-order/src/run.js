0c510cba82866899774736814eef21c3449e71ae
Daniel Ochenkowski <daniel.ochenkowski@gmail.com>
2024-10-25 05:37:09 +0200

    update

--- extensions/free-item-with-order/src/run.js
+++ extensions/free-item-with-order/src/run.js
@@ -10,7 +10,7 @@ import { DiscountApplicationStrategy } from "../generated/api";
  * @type {FunctionRunResult}
  */
 const EMPTY_DISCOUNT = {
-  discountApplicationStrategy: DiscountApplicationStrategy.First,
+  discountApplicationStrategy: DiscountApplicationStrategy.All,
   discounts: [],
 };
 
